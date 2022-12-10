7bc6cddad6e47821c2612744475a06e87b68fbfd
Orsu Venkata Krishnaiah <119290590+OrsuVenkataKrishnaiah1235@users.noreply.github.com>
2022-12-10 22:15:38 +0530

    Delete Python directory

--- Python/3.Squresdict.py
+++
@@ -1,5 +0,0 @@
-dic={}
-n=eval(input("Enter the range"))
-for i in range(1,n+1):
-    dic[i]=i*i
-print(dic)
--- Python/4.ltot.py
+++
@@ -1,4 +0,0 @@
-#List to tuple converter()
-l=[str(i) for i in input().split()]
-t=tuple(l)
-print(l,t,end="\n")
