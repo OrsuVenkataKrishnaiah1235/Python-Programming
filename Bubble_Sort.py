252e18c5bb47c2c38d8a4855b47a7aea649557a6
OrsuVenkataKrishnaiah1235 <orsuvenkatakrishnaiah1235@gmail.com>
2023-01-25 21:27:42 +0530

    I  just Need Some Changes

---
+++ Bubble_Sort.py
@@ -0,0 +1,8 @@
+def bubbleSort(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr)-1-i):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[
+                    j]
+    print(arr)
+bubbleSort([1,21,0,4,12,345,12])                
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Python_Practice
-Practiced Basic Level To high level Problem Statements
+# Python_Practice_Problems
+
+Here iam sharing all DSA ALGORITHMS And OOPS AND BASIC PROBLEMS Solutions:-
+
+Softwares used:-Visual Studio Code
